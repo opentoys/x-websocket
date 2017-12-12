5ab2ee05f65a1c94d480d0600d0125d587312e9d
xiaoxia <xiaqiubo123@163.com>
2017-12-12 15:44:42 +0800

    修改代码减少项目依赖,去除express使用原生http

--- libs/index.js
+++ libs/index.js
@@ -1,19 +1,16 @@
-const express = require("express")
-const app = express()
-const server = require("http").Server(app)
-const router = express.Router()
-const fs = require("fs")
+const server = require("http").createServer(handler)
+
 // 自定义事件用于当url访问时触发
 const urlEmitter  = require("./event")
 
 const io = require("socket.io")(server)
 
-router.use((req,res)=>{
+function handler(req,res){
     urlEmitter.emit("url",req.url)
     urlEmitter.on("urldata",(data)=>{
         res.end(data)        
     })
-})
+}
 
 io.on("connection",(socket)=>{
     console.log("client is connect")
@@ -29,7 +26,6 @@ io.on("disconnect",()=>{
     console.log("client is ending")
 })
 
-app.use(router)
 
 module.exports=server
 
